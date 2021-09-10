f14aacafe55becf918ee9adfd6efdb0d1237011c
CSI-Marcos-Perez <25-046@sanignacio.pr>
2021-09-10 14:33:10 -0400

    Module #4: What did you have for breakfast?

---
+++ Modules/Module4/Module4.py
@@ -0,0 +1,9 @@
+print("What did you have for breakfast?")
+meal = "French toast"
+print(meal)
+print("What did you have for lunch?")
+meal = "Chicken sandwich"
+print(meal)
+print("What did you have for dinner?")
+meal = "Sushi"
+print(meal)
