bbb5061d2b9da235cc2af040670f611613cde1d4
CSI-Marcos-Perez <25-046@sanignacio.pr>
2021-09-20 14:56:24 -0400

    Force = mass *acceleration

---
+++ Modules/Module4/Marcos-Pérez-Calculator.py
@@ -0,0 +1,10 @@
+mass = int(input("What is the mass?: "))
+acceleration = int(input("What is the acceleration?: "))
+print("The force is: ", mass * acceleration)
+
+print()
+def Force(mass, acceleration):
+    print(f"The force is {int(mass) * int(acceleration)}")
+print("Mass is 50")
+print("Acceleration is 20")
+Force(50, 20)
