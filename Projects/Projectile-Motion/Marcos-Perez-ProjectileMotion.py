407a4699cc36bb3b697742ed782d101eb878e556
CSI-Marcos-Perez <25-046@sanignacio.pr>
2021-10-28 14:34:32 -0400

    Projectile Motion Complete TODAY

--- Projects/Projectile-Motion/Marcos-Perez-ProjectileMotion.py
+++ Projects/Projectile-Motion/Marcos-Perez-ProjectileMotion.py
@@ -1,17 +1,20 @@
-sniper = "Orsis_T-5000"
-caliber = "7.62x51mmNATO"
-ammunition = "Single (Bolt Action)"
-velocity_ms = 600
+# import math
+# sniper = "Orsis_T-5000"
+# caliber = "7.62x51mm"
+# ammunition = "FMJ"
+# velocity_ms = 840
 
-building = "Caribbean Sea View"
-height = 334
+# building = "Caribbean Sea View"
+# height = 334
 
-gravity = 9.81
+# gravity = 9.81
 
-import math
+def ProjectileFunction(sniper:str, ammunition:str, velocity_ms:int, building:str, height:int, caliber:str, gravity:int):
+    time_s = math.sqrt((2 * height) / gravity) 
+    distance = (velocity_ms * time_s)
 
-time_s = math.sqrt((2 * height) / gravity)
+    print(f"The gun selected for the experiment is {sniper}. The caliber of the gun is {caliber}")
 
-distance = (velocity_ms * time_s)
+    print("The ORSIS T-5000 rifle is a sniper rifle that is known for its high-accuracy. This weapon was created thanks to the help of professional shooters. This gun is a hand-reloaded repeater with sliding breech bolt and two front locking lugs. This serves many purposes, as it has specifications that provide high-accuracy long-range shots. An estimate to the each shot's distnace is up to 1500m. The firing and recoil phases, and the gun can swiftly recover the sighting line.")
 
-print("The ORSIS T-5000 rifle is a sniper rifle that is known for its high-accuracy. This weapon was created thanks to the help of professional shooters. This gun is a hand-reloaded repeater with sliding breech bolt and two front locking lugs. This serves many purposes, as it has specifications that provide high-accuracy long-range shots. An estimate to the each shot's distnace is up to 1500m. The firing and recoil phases, and the gun can swiftly recover the sighting line.")
+ProjectileFunction("Orsis_t-5000", "FMJ", 840, "Caribbean Sea View", 334, "7.62x51mm", 9.81)
