21fc9e3151a191c96eca7fcd1cc4a818e934f002
CSI-Marcos-Perez <25-046@sanignacio.pr>
2022-01-28 15:08:36 -0400

    HANGMAN WORK

--- Projects/Marcos-Perez-Hangman.py
+++ Projects/Marcos-Perez-Hangman.py
@@ -6,7 +6,7 @@ def get_word(word_list):
     return word.upper()
 
 def play(word):
-    word_completion = "_" * len(word)
+    word_completion = "_ " * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
@@ -28,7 +28,33 @@ def play(word):
             else:
                 print("Nice one", guess, "is in the word!")
                 guessed_letters.append(guess)
-
+                word_as_list = list(word_completion)
+                indices = [i for i, lettoer in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                    word_completion = "".join(word_as_list)
+                    if "-" not in word_completion:
+                        guessed = True
+        elif len(guess) == len(word) and guess.isalpha():
+            if guess in guessed_words:
+                print("You already tried", guess, "!")
+            elif guess != word:
+                print(guess, "Is not the word 😂")
+                tries -= 1,
+                guessed_words.append(guess)
+            else:
+                guessed = True
+                word_completion = word
+        else:
+            print("Invalid input. Are you a hacker?")
+        print(display_hangman(tries))
+        print(word_completion)
+        print("\n")
+    
+    if guessed:
+        print("I'll get you next time Perry the Platypus!!!")
+    else:
+        print("Haha. YOU LOSE 🤣!!! The word was" + word + "You're really BAD. Play again, I dare you 🥱!")
 
 def display_hangman(tries):
     stages = [      """
@@ -95,3 +121,13 @@ def display_hangman(tries):
                         -
                         """,
     ]
+    return stages[tries]
+def main():
+    word = get_word(word_list)
+    play(word)
+    while input("You still haven't won? Going again? (Y/N)").upper() == "Y":
+        word = get_word(word_list)
+        play(word)
+
+if __name__== "__main__":
+    main()
