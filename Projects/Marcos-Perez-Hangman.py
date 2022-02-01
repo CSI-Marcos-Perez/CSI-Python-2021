f73d29c13da54dac25c57af4b71638ff5a6fea78
CSI-Marcos-Perez <25-046@sanignacio.pr>
2022-02-01 14:41:17 -0400

    COMPLETE HANGMAN FINALIZED

--- Projects/Marcos-Perez-Hangman.py
+++ Projects/Marcos-Perez-Hangman.py
@@ -6,7 +6,7 @@ def get_word(word_list):
     return word.upper()
 
 def play(word):
-    word_completion = "_ " * len(word)
+    word_completion = "-" * len(word)
     guessed = False
     guessed_letters = []
     guessed_words = []
@@ -29,18 +29,18 @@ def play(word):
                 print("Nice one", guess, "is in the word!")
                 guessed_letters.append(guess)
                 word_as_list = list(word_completion)
-                indices = [i for i, lettoer in enumerate(word) if letter == guess]
+                indices = [i for i, letter in enumerate(word) if letter == guess]
                 for index in indices:
                     word_as_list[index] = guess
-                    word_completion = "".join(word_as_list)
-                    if "-" not in word_completion:
-                        guessed = True
+                word_completion = "".join(word_as_list)
+                if "-" not in word_completion:
+                    guessed = True
         elif len(guess) == len(word) and guess.isalpha():
             if guess in guessed_words:
                 print("You already tried", guess, "!")
             elif guess != word:
-                print(guess, "Is not the word 😂")
-                tries -= 1,
+                print(guess, "It's not the word 😂")
+                tries -= 1
                 guessed_words.append(guess)
             else:
                 guessed = True
@@ -57,7 +57,7 @@ def play(word):
         print("Haha. YOU LOSE 🤣!!! The word was" + word + "You're really BAD. Play again, I dare you 🥱!")
 
 def display_hangman(tries):
-    stages = [      """
+    stages = [ """
                          --------
                         |   |
                         |   O
@@ -66,7 +66,7 @@ def display_hangman(tries):
                         |  / \\
                         -
                         """,
-                        """                        -------
+                        """                        
                          --------
                         |   |
                         |   O
@@ -75,7 +75,7 @@ def display_hangman(tries):
                         |  / 
                         -
                         """,
-                        """                        -------
+                        """                        
                          --------
                         |   |
                         |   O
@@ -84,7 +84,7 @@ def display_hangman(tries):
                         |   
                         -
                         """,
-                        """                        -------
+                        """                        
                          --------
                         |   |
                         |   O
@@ -93,7 +93,7 @@ def display_hangman(tries):
                         |   
                         -
                         """,
-                        """                        -------
+                        """                       
                          --------
                         |   |
                         |   O
@@ -101,25 +101,25 @@ def display_hangman(tries):
                         |   |
                         |   
                         -
-                        """
-                        """                        -------
+                        """,
+                        """                       
                          --------
                         |   |
                         |   O
                         |  
                         |  
-                        |  
-                        -
-                        """
-                        """                        -------
-                         --------
-                        |   |
-                        |  
-                        |  
-                        |   
                         |  
                         -
                         """,
+                        """                       
+                         --------
+                        |   |
+                        |  
+                        |  
+                        |   
+                        |  
+                        -
+                        """
     ]
     return stages[tries]
 def main():
@@ -129,5 +129,5 @@ def main():
         word = get_word(word_list)
         play(word)
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
