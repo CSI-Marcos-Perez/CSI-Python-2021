d650fe5c907d9a626bfa8a20e4d44e84f38fd60a
CSI-Marcos-Perez <25-046@sanignacio.pr>
2022-03-14 14:58:29 -0400

    WEB-SCRAPING TERMINADO

--- Modules/Module4/Module4.py
+++ Modules/Module4/Module4.py
@@ -1,9 +1,13 @@
-print("What did you have for breakfast?")
-meal = "French toast"
-print(meal)
-print("What did you have for lunch?")
-meal = "Chicken sandwich"
-print(meal)
-print("What did you have for dinner?")
-meal = "Sushi"
-print(meal)
+# print("What did you have for breakfast?")
+# meal = "French toast"
+# print(meal)
+# print("What did you have for lunch?")
+# meal = "Chicken sandwich"
+# print(meal)
+# print("What did you have for dinner?")
+# meal = "Sushi"
+ #print(meal)
+
+myName:str = "Marcos"
+food:str = "pizza"
+print(f"Hello, my name is {myName} and I like to eat {food}")
